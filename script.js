ad1e4eb77a1a86554267ff215858a489362f956e
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-06-03 12:19:37 +0000

    Added bable  and react cdn

---
+++ script.js
@@ -0,0 +1,9 @@
+// let reactImg=React.createElement('img', {src:"https://files.codingninjas.in/coding-ninjas-24647.png", width:400});
+// ReactDOM.createRoot(document.getElementById('root')).render(reactImg);
+
+
+let jsxHeading=(<h1>JSX</h1>);
+ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+
+
+
