67a25f7150529748c05de498c0365c8006f6fa47
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-06-12 13:45:37 +0000

    practiced arrays and objects traversal

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 // let reactImg=React.createElement('img', {src:"https://files.codingninjas.in/coding-ninjas-24647.png", width:400});
 // ReactDOM.createRoot(document.getElementById('root')).render(reactImg);
 
+let arr=[
+    {
+        name:"Aryan",
+        email:"aryan@gmail.com"
+    },
+    {
+        name:"vishu",
+        email:"vishu@gmail.com"
+    },
+    {
+        name:"rbdii",
+        email:"rbdii@gmail.com"
+    },
+    {
+        name:"nayak",
+        email:"nayak@gmail.com"
+    },
+]
+let jsxHeading=(
+       <table>
+             <tr>
+                 <th>Name</th>
+                <th>Email</th>
+             </tr>
+             {
+                arr.map((student)=>(
+                        <tr>
+                             <td>{student.name}</td>
+                             <td>{student.email}</td>
+                        </tr>
+                    )
+                )
+             }
+       </table>
+);
 
-let jsxHeading=(<h1>JSX</h1>);
 ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
 
 
