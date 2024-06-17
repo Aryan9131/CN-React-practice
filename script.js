a1269a9efe353b537525c698aba6661b8e1744e1
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-06-17 07:20:03 +0000

    Added scoreboard

--- script.js
+++ script.js
@@ -19,25 +19,26 @@ let arr=[
         email:"nayak@gmail.com"
     },
 ]
-let jsxHeading=(
-       <table>
-             <tr>
-                 <th>Name</th>
-                <th>Email</th>
-             </tr>
-             {
-                arr.map((student)=>(
-                        <tr>
-                             <td>{student.name}</td>
-                             <td>{student.email}</td>
-                        </tr>
-                    )
-                )
-             }
-       </table>
+let score=0;
+let wickets=0;
+let addScore=(val)=>{
+    score+=val;
+    rootElement.render(<App />);
+}
+const App=()=>(
+       <>
+         <h1>Score : {score}/ {wickets}</h1>
+         <button onClick={()=>addScore(1)}>1</button>
+         <button onClick={()=>addScore(2)}>2</button>
+         <button onClick={()=>addScore(3)}>3</button>
+         <button onClick={()=>addScore(4)}>4</button>
+         <button onClick={()=>addScore(5)}>5</button>
+         <button onClick={()=>addScore(6)}>6</button>
+       </>
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+const rootElement= ReactDOM.createRoot(document.getElementById('root'))
+rootElement.render(<App />);
 
 
 
