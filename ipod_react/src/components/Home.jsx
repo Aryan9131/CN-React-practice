437fbe935d80bcf5c5233e136a73bdaf2cb94b50
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-09-17 12:31:25 +0000

    Added home page

---
+++ ipod_react/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box';
+
+
+export const Home=()=>{
+    return (
+        <Box sx={{backgroundColor:"green", width:"100%", height:"100%"}}>
+            <h3>Hello home page</h3>
+        </Box>
+    )
+}
